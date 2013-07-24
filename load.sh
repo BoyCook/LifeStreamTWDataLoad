f7f44d3c665e28b9927520ec7bb01e6bac52d189
Craig Cook <boycook@me.com>
2013-07-24 11:11:15 +0100

    Moved extra github files to more

--- load.sh
+++ load.sh
@@ -2,10 +2,15 @@
 
 HOST=127.0.0.1:8080
 
-# Create bags - todo use load_dir
-curl -X PUT -H 'Content-Type: application/json' -d @bags/tweets.json http://$HOST/bags/tweets
-curl -X PUT -H 'Content-Type: application/json' -d @bags/blogs.json http://$HOST/bags/blogs
-curl -X PUT -H 'Content-Type: application/json' -d @bags/blogs.json http://$HOST/bags/static
+# ContentType - File - Bag
+function load_bag() {	
+	curl -X PUT -H "Content-Type: $1" --data-binary "@$2" http://$HOST/bags/$3
+}
+
+# ContentType - File - Bag - Name
+function load_tiddler() {	
+	curl -X PUT -H "Content-Type: $1" --data-binary "@$2" http://$HOST/bags/$3/tiddlers/$4
+}
 
 function load_dir() {	
 	DIR=$1
@@ -23,23 +28,28 @@ function load_dir() {
 	cd ../
 }
 
-# ContentType - File - Bag - Name
-function load_file() {	
-	curl -X PUT -H "Content-Type: $1" --data-binary "@$2" http://$HOST/bags/$3/tiddlers/$4
+function load_statics() {
+	load_tiddler image/jpeg static/images/craig.jpg static craig.jpg
+
+	load_tiddler application/vnd.ms-fontobject static/css/Elusive-Icons.eot static Elusive-Icons.eot
+	load_tiddler image/svg+xml static/css/Elusive-Icons.svg static Elusive-Icons.svg
+	load_tiddler application/x-font-ttf static/css/Elusive-Icons.ttf static Elusive-Icons.ttf
+	load_tiddler application/octet-stream static/css/Elusive-Icons.woff static Elusive-Icons.woff
+
+	load_tiddler text/css static/css/default.css static default.css
+	load_tiddler text/css static/css/layout.css static layout.css
+	load_tiddler text/css static/css/elusive-webfont.css static elusive-webfont.css
+	load_tiddler text/javascript static/js/default.js static default.js
 }
 
-# load_dir bags 6
+function load_bags() {
+	load_bag application/json bags/tweets.json tweets
+	load_bag application/json bags/blogs.json blogs
+	load_bag application/json bags/static.json static
+}
+
+load_bags
+load_statics
 load_dir tweets 23
 load_dir blogs 7
 
-load_file image/jpeg static/images/craig.jpg static craig.jpg
-
-load_file application/vnd.ms-fontobject static/css/Elusive-Icons.eot static Elusive-Icons.eot
-load_file image/svg+xml static/css/Elusive-Icons.svg static Elusive-Icons.svg
-load_file application/x-font-ttf static/css/Elusive-Icons.ttf static Elusive-Icons.ttf
-load_file application/octet-stream static/css/Elusive-Icons.woff static Elusive-Icons.woff
-
-load_file text/css static/css/default.css static default.css
-load_file text/css static/css/layout.css static layout.css
-load_file text/css static/css/elusive-webfont.css static elusive-webfont.css
-load_file text/javascript static/js/default.js static default.js
